82b1185036690cd8ed0acdbfad05023e189abce2
Vladimir <shokilak@gmail.com>
2023-05-24 12:46:14 +0600

    feat: added app's entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './application/controllers/app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './application/controllers/users.controller';
+import { AuthController } from './application/controllers/auth.controller';
+import { PostsController } from './application/controllers/posts.controller';
+import { UsersService } from './domain/users/users.service';
+import { PostsService } from './domain/posts/posts.service';
+import { AuthService } from './domain/auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, AuthController, PostsController],
+  providers: [AppService, UsersService, PostsService, AuthService],
 })
 export class AppModule {}
